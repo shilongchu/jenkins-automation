a201413b63fb2951fe5481562cb386581615fba9
Shilong Chu <shilongchu@yahoo.com>
2019-03-16 13:02:58 -0400

    only base job

--- jobs/jobs.groovy
+++ jobs/jobs.groovy
@@ -34,18 +34,6 @@ def pipelineScript = """
     } 
 """ 
 
- 
-new PipelineJobBuilder( 
-        name: 'Hello-Pipeline-Job', 
-        description: 'Example Pipeline job', 
-        pipelineScript: pipelineScript, 
-        sandboxFlag: false 
-).build(this).with { 
-    // example of extending a job with the job-dsl api 
-    logRotator { 
-        numToKeep(60) 
-    } 
-} 
 
 new BaseJobBuilder( 
         name: "Hello-Freestyle-Job", 
